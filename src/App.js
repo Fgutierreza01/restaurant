589435bb1029e106e33d9d8d5d8a78e36f3d7565
Fgutierreza01 <fgutierreza01@gmail.com>
2022-10-08 15:55:18 -0300

    instalacion y configuracion firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
-
-function App() {
-  return <div>hola</div>
+import {Routes, Route, Navigate} from 'react-router-dom'
+import Home from './views/Home';
+import Contacto from './views/Contacto';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Fragment } from 'react';
+import CustomNavbar from './components/CustomNavbar';
+import Footer from './components/Footer';
+const App = () => {
+  return (
+    <Fragment>
+     <CustomNavbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/contacto' element={<Contacto/>}/>
+      <Route path='*' element={<Navigate to='/'/>}/>
+    </Routes>
+    <Footer/>
+    </Fragment>
+  )
+  
 }
 
 export default App;
---
+++ src/components/CustomNavbar.js
@@ -0,0 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import {Link} from 'react-router-dom'
+
+function CustomNavbar() {
+  return (
+    <Container>
+      <Navbar expand="lg" variant="light" bg="light">
+        <Container>
+          <Navbar.Brand >Nombre Restaurant</Navbar.Brand>
+          <Link to="/">Home</Link>
+          <Link to="/contacto">Contacto</Link>
+        </Container>
+      </Navbar>
+    </Container>
+  );
+}
+
+export default CustomNavbar;
